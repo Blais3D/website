44361513a81d5fc0f01ad05f2b6557dd03220c08
Blais3D <LinwoodBlaisdell@gmail.com>
2022-03-04 00:07:41 -0500

    yoooooooooo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let color = "black";
+  if (count > 5) {
+    color = "red";
+  }
+
+  const color2 = count > 5 ? "red" : "black";
 
   return (
     <div className={styles.container}>
@@ -13,12 +19,15 @@ const Home: NextPage = () => {
         <title>Hello Avery</title>
       </Head>
       <h1>Hello Avery Click The Button</h1>
-      {count}
-      <button onClick={()=> setCount(count + 1)}>
+      <h2>{count}</h2>
+      <button
+        style={{ border: `2px solid ${color2}` }}
+        onClick={() => setCount(count + 1)}
+      >
         Increase
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
