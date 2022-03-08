3f04594c2f447df21109f78e5de4e22c6727e999
Blais3D <LinwoodBlaisdell@gmail.com>
2022-03-07 22:13:47 -0500

    Red Green BLue picker

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,12 @@ import { brotliDecompressSync } from "zlib";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const [count, setCount] = useState(0);
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
 
-  const color = "rgb(" + (count % 255) + ", " + 150 + ", " + 150 + ")";
+  const color =
+    "rgb(" + (red % 255) + ", " + (green % 255) + ", " + (blue % 255) + ")";
 
   return (
     <div className={styles.container} style={{ background: `${color}` }}>
@@ -19,8 +22,12 @@ const Home: NextPage = () => {
         <h1>Thank you Theo for teaching me this stuff</h1>
         <h1>Hello Sam, see you soon!</h1>
         <h1>Hey Julia!</h1>
-        <h2>{count}</h2>
-        <button onClick={() => setCount(count + 5)}>Increase</button>
+        <h2>Percent red value: {(red % 255) / 255}</h2>
+        <h2>Percent green value: {(green % 255) / 255}</h2>
+        <h2>Percent blue value: {(blue % 255) / 255}</h2>
+        <button onClick={() => setRed(red + 5)}>Increase Red</button>
+        <button onClick={() => setGreen(green + 5)}>Increase Green</button>
+        <button onClick={() => setBlue(blue + 5)}>Increase Blue</button>
       </body>
     </div>
   );
