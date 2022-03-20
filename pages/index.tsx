6225013c06c04d45657739b11224bde6d509a540
Blais3D <LinwoodBlaisdell@gmail.com>
2022-03-20 13:48:54 -0400

    IDEK

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,39 @@
+import { link } from "fs";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
 import { useState } from "react";
 import { brotliDecompressSync } from "zlib";
-import styles from "../styles/Home.module.css";
+
+const MyButton: React.FC<{ text: String; color: string; link: any }> = (
+  props
+) => {
+  return (
+    <button style={{ background: props.color }} onClick={props.link}>
+      {props.text}
+    </button>
+  );
+};
 
 const Home: NextPage = () => {
   return (
-    <html className={styles.html}>
+    <html className="bg-black">
       <head>
         <title>Welcome!</title>
       </head>
-      <body className={styles.bodyHeader}>
-        <img src="public\BlaisLogo.png" alt="logo"></img>
-      </body>
-      <body className={styles.body}>
-        <h1 className={styles.h1}>Welcome To Blais.gg!</h1>
+      <body>
+        <div>
+          <div>
+            <img src="/BlaisLogo.png" alt="logo"></img>
+            <h1>Blais</h1>
+          </div>
+          <div>
+            <button>Blog</button>
+            <button>Portfolio</button>
+            <button>Socials</button>
+          </div>
+        </div>
       </body>
     </html>
   );
